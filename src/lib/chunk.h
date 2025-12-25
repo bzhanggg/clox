6e13786297e14c7b2b07273cee6f5b8761490c51
bzhanggg <blz19@scarletmail.rutgers.edu>
2025-12-25 17:22:35 -0500

    set up cmake

---
+++ src/lib/chunk.h
@@ -0,0 +1,16 @@
+#ifndef CLOX_CHUNK_H_
+#define CLOX_CHUNK_H_
+
+#include <vector>
+
+namespace clox {
+namespace lib {
+
+enum class OpCode {
+    OP_RETURN
+};
+
+}
+}
+
+#endif // CLOX_CHUNK_H_
---
+++ src/lib/utiltypes.h
@@ -0,0 +1,6 @@
+#ifndef CLOX_UTILTYPES_H_
+#define CLOX_UTILTYPES_H_
+
+#include <stddef.h>
+
+#endif // CLOX_UTILTYPES_H_
